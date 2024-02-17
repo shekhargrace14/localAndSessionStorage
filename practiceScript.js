cbf89a8be7a7b45d7ade80f755e8364835be1a54
shekhargrace14 <shekhargrace14@gmail.com>
2024-02-17 17:00:17 +0530

    practice js

--- practiceScript.js
+++ practiceScript.js
@@ -5,11 +5,58 @@ form.addEventListener("submit",(event)=>{
     let name = event.target.name.value;
     let phone = event.target.phone.value;
     let email = event.target.email.value;
+    
+    let userData = {
+        UserName : name,
+        userPhone : phone,
+        userEmail : email,
+    }
+
+    let oldData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+    
+    oldData.push(userData)
+
+    localStorage.setItem("userDetails", JSON.stringify(oldData)) ;    
+
+    // console.log(JSON.parse(localStorage.getItem("userDetails")));
+
+    // localStorage.removeItem("userDetails")
+
+    
+    event.preventDefault();
+    displayData()
 })
 
-let userData = {
-    name : name,
-    phone : phone,
-    email : email,
+let displayData=()=>{
+    let userDisplayData = JSON.parse(localStorage.getItem("userDetails")) ?? [];  
+    // console.log(userDisplayData)
+    let main = document.querySelector(".main")
+    userDisplayData.forEach((element, i)=>{
+        main.insertAdjacentHTML("afterbegin",`
+        <div class="column card">   
+            <span onclick="removeData(${i})" >&#10006;</span>
+            <h5>Name</h5>
+            <p>${element.UserName}</p>
+            <h5>Phone</h5>
+            <p>${element.userPhone}</p>
+            <h5>Email</h5>
+            <p>${element.userEmail}</p>
+        </div>
+        `)
+    })
 }
-console.log()
+
+let removeData=(index)=>{
+    let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+    
+    userData.splice(index,1)
+
+    localStorage.setItem("userDetails", JSON.stringify(userData))
+    
+    console.log(index, "this is index");
+    displayData()
+}
+// displayData()
+
+
+
